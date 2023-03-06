d40c208cb513ec7f8ab1b9a208c838327d3c1301
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-03-06 17:44:00 +0100

    synchronizing the paddles

--- server.js
+++ server.js
@@ -11,6 +11,22 @@ server.listen(PORT, () => {
   console.log(`server is listening on PORT: ${PORT}`);
 });
 
+let readyPlayerCount = 0;
+
 io.on('connection', (socket) => {
-  console.log('user connected');
-})
+  console.log('user connected', socket.id);
+
+  socket.on('ready', () => {
+    console.log('user with id is ready ', socket.id);
+
+    readyPlayerCount++;
+
+    if(readyPlayerCount === 2) {
+      io.emit('startGame', socket.id)
+    }
+  });
+
+  socket.on('paddleMove', (paddleData) => {
+    socket.broadcast.emit('paddleMove', paddleData)
+  })
+});
