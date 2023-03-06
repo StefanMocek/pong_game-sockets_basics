fc3487122f32c296e3e3aaa6998840769ffcb6ba
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-03-06 21:28:11 +0100

    rooms added

--- sockets.js
+++ sockets.js
@@ -3,28 +3,33 @@ let readyPlayerCount = 0;
 function listen(io) {
   const pongNamespace = io.of('/pong');
   pongNamespace.on('connection', (socket) => {
+    let room;
     console.log('user connected', socket.id);
 
     socket.on('ready', () => {
-      console.log('user with id is ready ', socket.id);
+      room = 'room' + Math.floor(readyPlayerCount/2);
+      socket.join(room)
+
+      console.log('user with id is ready ', socket.id, room);
 
       readyPlayerCount++;
 
       if(readyPlayerCount % 2 === 0) {
-        pongNamespace.emit('startGame', socket.id)
+        pongNamespace.in(room).emit('startGame', socket.id)
       }
     });
 
     socket.on('paddleMove', (paddleData) => {
-      socket.broadcast.emit('paddleMove', paddleData)
+      socket.to(room).emit('paddleMove', paddleData)
     });
 
     socket.on('ballMove', (ballData) => {
-      socket.broadcast.emit('ballMove', ballData)
+      socket.to(room).emit('ballMove', ballData)
     });
 
     socket.on('disconnect', (reason) => {
       console.log(`Client ${socket.id} disconnected ${reason}`);
+      socket.leave(room);
     })
   })
 };
